c1f421860d044f6a4cf7282ac2ae450cf5e036f2
unknown <gron1gh1@ajou.ac.kr>
2020-03-11 17:19:35 +0900

    Add cluster

--- api_server.js
+++ api_server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 function normalize(min, max) {
   var delta = max - min;
   return function (val) {
-      return (val - min) / delta;
+    return (val - min) / delta;
   };
 }
 
@@ -22,60 +22,49 @@ function normalizeArray(array) {
   var maxValue = Math.max(...array);
   return array.map(normalize(minValue, maxValue));
 }
-app.get('/city', function (req, res,next) {
+app.get('/city', function (req, res, next) {
   request('http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=13', function (error, response, body) {
-      const $ = cheerio.load(body);
-      
-     
+    const $ = cheerio.load(body);
 
-      const $data2 = $("div.container div.content div.maplist");
-      var info = $data2.text().replace(/[\n\t]/g,'');
-      var res_arr = [];
-      info = info.split('명');
-      info = info.map(v => v.replace('확진환자수',''));
-      info.forEach((v,i) =>{
-        if(i % 2 == 0){
-        var city_name = v.substr(0,2);
-          city_name = city_name.replace('충남','chungnam');
-          city_name = city_name.replace('제주','jeju');
-          city_name = city_name.replace('경남','gyeongnam');
-          city_name = city_name.replace('경북','gyeongbuk');
-          city_name = city_name.replace('전북','jeonbuk');
-          city_name = city_name.replace('충북','chungbuk');
-          city_name = city_name.replace('강원','gangwon');
-          city_name = city_name.replace('경기','gyeonggi');
-          city_name = city_name.replace('전남','jeonnam');
-          city_name = city_name.replace('울산','ulsan');
-          city_name = city_name.replace('부산','busan');
-          city_name = city_name.replace('대구','daegu');
-          city_name = city_name.replace('대전','daejeon');
-          city_name = city_name.replace('인천','incheon');
-          city_name = city_name.replace('서울','seoul');
-          city_name = city_name.replace('광주','gwanju');
-          city_name = city_name.replace('세종','sejong');
-          res_arr.push(
-            {
-              city: city_name,
-              count: parseInt(v.substring(2)),
-              death_count: 0
-            }
-          );
-        }
-        else{
-          res_arr[res_arr.length - 1]['death_count'] = parseInt(v.substring(4));
+    var res_arr = [];
+    const $data = $('div.content div.data_table tbody');
+    for (var idx = 1; idx <= 18; idx++) {
+      var $info = $data.children(`tr:nth-child(${idx})`);
+      var city_name = $info.children('th').text();
+      city_name = city_name.replace('충남', 'chungnam');
+      city_name = city_name.replace('제주', 'jeju');
+      city_name = city_name.replace('경남', 'gyeongnam');
+      city_name = city_name.replace('경북', 'gyeongbuk');
+      city_name = city_name.replace('전북', 'jeonbuk');
+      city_name = city_name.replace('충북', 'chungbuk');
+      city_name = city_name.replace('강원', 'gangwon');
+      city_name = city_name.replace('경기', 'gyeonggi');
+      city_name = city_name.replace('전남', 'jeonnam');
+      city_name = city_name.replace('울산', 'ulsan');
+      city_name = city_name.replace('부산', 'busan');
+      city_name = city_name.replace('대구', 'daegu');
+      city_name = city_name.replace('대전', 'daejeon');
+      city_name = city_name.replace('인천', 'incheon');
+      city_name = city_name.replace('서울', 'seoul');
+      city_name = city_name.replace('광주', 'gwanju');
+      city_name = city_name.replace('세종', 'sejong');
+      var city_info = {
+        city: city_name,
+        prev_day: $info.children('td:nth-child(2)').text(),
+        count: $info.children('td:nth-child(3)').text(),
+        death_count: $info.children('td:nth-child(5)').text()
+      }
+      res_arr.push(city_info);
+    }
+    res_arr.sort((a,b) => a.count - b.count);
+    console.log(res_arr);
+    
+    res.json(res_arr)
 
-        }
-      })
-      res_arr.pop();
-      res_arr.sort((a,b) => a.count - b.count);
-
-      console.log(res_arr);
-      res.json(res_arr)
+  });
 
 });
- 
-});
 
-var server = app.listen(3001, function(){
+var server = app.listen(3001, function () {
   console.log("API Server Port 3001 on.")
 })
--- src/App.js
+++ src/App.js
@@ -1,83 +1,113 @@
-import React,{useState,useEffect,useRef} from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import logo from './logo.svg';
 import MapSVG from './data/korea_map.svg';
 import './App.css';
-import {ReactSVG} from 'react-svg';
+import { ReactSVG } from 'react-svg';
 import axios from 'axios';
-
-function Map()
-{
-  var [data,SetData] = useState(null);
+import skmeans from 'skmeans';
+function Map() {
+  var [data, SetData] = useState(null);
 
   useEffect(() => {
-   // axios.get('http://localhost:3001/city')
-	  axios.get("http://covidapi.run.goorm.io/city")
-    .then(function (response) {
-      SetData(response.data)
-      console.log(data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-    .finally(function () {
-      // always executed
-    });  
-  },[]);
+    axios.get('http://localhost:3001/city')
+      //axios.get("http://covidapi.run.goorm.io/city")
+      .then(function (response) {
+        SetData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        // always executed
+      });
+  }, []);
 
-	
+
   var data_render = <span>not</span>;
-  if(data != null)
-      data_render = <span>{data}</span>; 
+  if (data != null)
+    data_render = <span>{data}</span>;
   return (
-    
-    
+
+
     <div>
-     <span>{JSON.stringify(data)}</span>
+      <span>{JSON.stringify(data)}</span>
       <ReactSVG src={MapSVG} beforeInjection={
         svg => svg_data_load(svg)
-  }/>
+      } />
     </div>
   )
-  function svg_data_load(svg)
-	{
-         var all_city = [];
-          all_city = Array.from(svg.querySelectorAll('polyline'));
-		  all_city = all_city.concat(Array.from(svg.querySelectorAll('path')));
+  
+  function svg_data_load(svg) {
+    var all_city = [];
+    all_city = Array.from(svg.querySelectorAll('polyline'));
+    all_city = all_city.concat(Array.from(svg.querySelectorAll('path')));
+
+   
+    if (data) {
+      var red_palette = ['#ff4c4c','#ff2626','#ff0000','#b30000','#660000'];
+      var cluster_data = data.map(v => parseInt(v.count));
+      cluster_data.pop();
+      cluster_data = skmeans(cluster_data,5,'kmrand').idxs;
+      console.log(cluster_data);
+      data.map(function(v,i){
+        v.cluster = red_palette[cluster_data[i]];
+        return v;
+      })
+      console.log(data);
 
 
-       if(data){
-          var EntireCount = data[data.length - 1]['death_count'];
-		   
-          data.forEach(function(elem,i){
-			  
-			  var find_elem;
-				  all_city.some((v)=>{
-                   if(v.id.includes(elem.city))
-					{
-                         find_elem=v;
-						return true;
-                    }
-              });
 
-            if(find_elem)
-              {
-                var color = parseInt(((data.count-1) / EntireCount) * 100);
-                var percent = (17*50/i);
-                find_elem.setAttribute('style',`fill:hsl(0,100%,${percent+'%'});stroke:lightgray`);
-                find_elem.onclick = () => alert(find_elem.id);
-                find_elem.onmouseenter = () => find_elem.setAttribute('style',`fill:hsl(0,100%,${(percent + 10)+'%'});stroke:lightgray`);
-                find_elem.onmouseleave = () =>  find_elem.setAttribute('style',`fill:hsl(0,100%,${percent+'%'});stroke:lightgray`);
-              
-              }
-            });
+
+      var EntireCount = data[data.length - 1]['count'];
+     // var avg = get_avg(data,data.length);
+      // console.log(avg);
+      //
+      var mid_val = data[data.length / 2]['count'];
+
+      var partial_up = data.filter(v => v.count > mid_val && v.count != EntireCount);
+      var partial_up_count = partial_up.reduce((a,b)=>{
+        if(a.count == undefined)
+          return parseInt(a) + parseInt(b.count);
+        return parseInt(a.count) + parseInt(b.count);
+      });
+      console.log(partial_up_count);
+      var partial_down = data.filter(v => v.count <= mid_val);
+      var partial_down_count = partial_down.reduce((a,b)=>{
+        if(a.count == undefined)
+          return parseInt(a) + parseInt(b.count);
+        return parseInt(a.count) + parseInt(b.count);
+      });
+      console.log(partial_down_count);
+       console.log(partial_up_count);
+      data.forEach(function (elem, i) {
+
+        var find_elem;
+        all_city.some((v) => {
+          if (v.id.includes(elem.city)) {
+            find_elem = v;
+            return true;
+          }
+        });
+
+        if (find_elem) {
+          var percent = elem.count * 100 / EntireCount;
+          //hsl(0,100%,50%) ~ hsl(0,100%,100%) = red ~ white;
+          console.log(elem.city + ':' + percent);
+          find_elem.setAttribute('style', `fill:${elem.cluster};stroke:lightgray`);
+          find_elem.onclick = () => alert(find_elem.id);
+          find_elem.onmouseenter = () => find_elem.setAttribute('style',  `fill:${elem.cluster};stroke:lightgray`);
+          find_elem.onmouseleave = () => find_elem.setAttribute('style',  `fill:${elem.cluster};stroke:lightgray`);
+
+        }
+      });
     }
- 
-}
+
+  }
 }
 function App() {
-  
+
   return (
-    <Map/>
+    <Map />
   );
 }
 
