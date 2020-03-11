35c9004d65cc784d2fcbc50172d6ef433d41098c
unknown <gron1gh1@ajou.ac.kr>
2020-03-11 20:19:51 +0900

    Add Item Func

--- src/CovidMap.js
+++ src/CovidMap.js
@@ -4,9 +4,24 @@ import axios from 'axios';
 import skmeans from 'skmeans';
 import MapSVG from './data/korea_map.svg';
 
+function CovidItem(props)
+{
+    const box_style={
+        position: 'absolute',
+        width:"100px",
+        height:"100px",
+        boder:"1px solid black",
+        background: props.color
+    }
+    return (
+        <div style={box_style}>
+
+        </div>
+    )
+}
 function CovidMap() {
     var [data, SetData] = useState(null);
-  
+    var [render,SetRender] = useState('');
     useEffect(() => {
       axios.get('http://localhost:3001/city')
         //axios.get("http://covidapi.run.goorm.io/city")
@@ -18,15 +33,26 @@ function CovidMap() {
         })
        
     }, []);
-  
+
+    function ShowComponent()
+    {
+        switch(render){
+            case 'daegu': return <CovidItem color='black'/>
+            case 'gyeongbuk' : return <CovidItem color='skyblue'/>
+        }
+    }
     return (
       <div class='screen'>
+        {ShowComponent()}
         <ReactSVG src={MapSVG} beforeInjection={
           svg => svg_data_load(svg)
         } />
       </div>
     )
-    
+    function show_item(id)
+    {
+        SetRender(id)
+    }
     function svg_data_load(svg) {
       var all_city = [];
       all_city = Array.from(svg.querySelectorAll('polyline'));
@@ -49,11 +75,10 @@ function CovidMap() {
             if (dom_elem.id.includes(data_elem.city)) {
         
                 dom_elem.setAttribute('style', `fill:${red_palette[data_elem.cluster]};opacity:1`);
-                dom_elem.onclick = () => alert(dom_elem.id);
+                dom_elem.onclick = () => console.log(dom_elem.getBoundingClientRect());
                 dom_elem.onmouseenter = () => dom_elem.setAttribute('style',  `fill:${red_palette[data_elem.cluster]};opacity:0.5;transition:0.3s`);
                 dom_elem.onmouseleave = () => dom_elem.setAttribute('style',  `fill:${red_palette[data_elem.cluster]};opacity:1;transition:0.3s`);
-     
-              return true;
+                return true;
             }
           });
           
